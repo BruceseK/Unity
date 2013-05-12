b836ec0fcfafc0f67553c906be03156e3f272a7a
ChiroM <brucesek@gmail.com>
2013-05-11 23:28:35 -0400

    Initial Commit

---
+++ testfile.cpp
@@ -0,0 +1,16 @@
+using namespace std;
+
+#include <iostream>
+#include <iomanip>
+
+int main()
+{
+	int X;
+	int arr [10];
+
+	for( int i = 0; i <= 9; ++i )
+	{
+		arr[i] = X;
+		cout << arr[i] << endl;
+	}
+}
